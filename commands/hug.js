70a8d7da00fa80bd053e14b9419d03a798a9a8b7
daadyrbarn <daadyrbarn@gmail.com>
2018-12-30 23:10:15 +0100

    keep it simple!

--- commands/hug.js
+++ commands/hug.js
@@ -1,20 +1,20 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+// const Discord = require('discord.js');
+// const client = new Discord.Client();
 
 module.exports = {
 	name: 'hug',
 	description: 'Want a hug?',
 	args: true,
 	execute(message, args) {
-		const hugger = message.author;
+		// const hugger = message.author;
 		const huggee = args[0];
 
 		if (huggee === 'me') {
 			message.react('🤗');
 		}
 		else {
-			const userID = client.users.get('name', huggee).id;
-			message.channel.send(`${hugger} wants you to have a hug @${userID} :hugging:`);
+			// const userID = client.users.get('name', huggee).id;
+			message.channel.send(`This hug is for @${huggee}! :hugging:`);
 		}
 	},
 };
