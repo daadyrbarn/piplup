1537bd97e17a326afe9078594ab1b70fb9e6d7e9
daadyrbarn <daadyrbarn@gmail.com>
2018-12-30 23:42:27 +0100

    create date object

--- commands/next.js
+++ commands/next.js
@@ -4,14 +4,13 @@ module.exports = {
 	name: 'next',
 	description: 'Info about next season!',
 	execute(message, args) {
-		const date = '2019-01-15';
+		const date = new date(2019, 0, 15);
 		const countdown = date - Date.now();
 		const info = new Discord.RichEmbed()
 			.setColor('#0099ff')
 			.setAuthor('Piplup', 'https://vignette.wikia.nocookie.net/pkmnshuffle/images/1/11/Piplup.png')
 			.addField('Pip pip!', `Next season starts in ${countdown} days!`)
-			.addField('Pip pip!', 'If you haven\'t already signed up, you can do it [here](https://goo.gl/forms/rSn7n3byrdKRIPgq1)!')
-			.setTimestamp();
+			.addField('', 'If you haven\'t already signed up, you can do it [here](https://goo.gl/forms/rSn7n3byrdKRIPgq1)!');
 		message.channel.send(info);
 	},
 };
