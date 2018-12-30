4726818d446b003f36ff848fd6238d06ccc0547b
daadyrbarn <daadyrbarn@gmail.com>
2018-12-30 22:16:20 +0100

    playing with hug

--- commands/hug.js
+++ commands/hug.js
@@ -1,14 +1,23 @@
+const Discord = require('discord.js');
+const guild = Discord.Guild;
+
+
 module.exports = {
 	name: 'hug',
 	description: 'Want a hug?',
 	args: true,
 	execute(message, args) {
+		const hugger = message.author;
 		const huggee = args[0];
+		guild.Fetchmembers(huggee)
+			.then(console.log)
+			.catch(console.error);
+
 		if (huggee === 'me') {
 			message.react('🤗');
 		}
 		else {
-			message.channel.send(`@${huggee} :hug:`);
+			message.channel.send(`${hugger} wants you to have a hug @${huggee} :hugging:`);
 		}
 	},
 };
