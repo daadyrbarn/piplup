8197aa4dabfd487047b816b2f4f8bddb049ca9b2
daadyrbarn <daadyrbarn@gmail.com>
2018-12-31 00:20:10 +0100

    do math!

--- commands/next.js
+++ commands/next.js
@@ -4,14 +4,14 @@ module.exports = {
 	name: 'next',
 	description: 'Info about next season!',
 	execute(message, args) {
-		const date = new Date('January 15, 2019 22:00:00');
+		const date = new Date('January 15, 2019 22:00:00 GMT+01:00');
 		const now = Date.now();
-		const countdown = date - now;
+		const msLeft = date - now;
+		const daysLeft = Math.roof(msLeft / (60 * 60 * 24 * 1000));
 		const info = new Discord.RichEmbed()
 			.setColor('#0099ff')
 			.setAuthor('Piplup', 'https://vignette.wikia.nocookie.net/pkmnshuffle/images/1/11/Piplup.png')
-			.addField('Pip pip!', `Next season starts in ${countdown} days!`)
-			.addField('If you haven\'t already signed up, you can do it [here](https://goo.gl/forms/rSn7n3byrdKRIPgq1)!');
+			.addField('Pip pip!', `Next season starts in ${daysLeft} days!\nIf you haven't already signed up, you can do it [here](https://goo.gl/forms/rSn7n3byrdKRIPgq1)!`);
 		message.channel.send(info);
 	},
 };
