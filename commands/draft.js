07e22234230ebdee786615c8225a5f795c55ec9c
daadyrbarn <daadyrbarn@gmail.com>
2019-01-09 01:23:56 +0100

    remove line break

--- commands/draft.js
+++ commands/draft.js
@@ -6,7 +6,7 @@ module.exports = {
 	execute(message, args) {
 		const info = new Discord.RichEmbed()
 			.setColor('#0099ff')
-			.addField('Pip pip!', 'Drafting for the next season\'s ranked divisions will take place Saturday 12/1 starting at 17:00.\n')
+			.addField('Pip pip!', 'Drafting for the next season\'s ranked divisions will take place Saturday 12/1 starting at 17:00.')
 			.addField('Location', 'The draft will take place at Trine\'s house! Here\'s a handy link with [directions](https://www.google.com/maps/dir/?api=1&destination=Hans+Broges+Gade+43,+8000+Aarhus)!');
 		message.channel.send(info);
 	},
