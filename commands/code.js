adf58d5321aa9700c4374f0cb5e0c83e83c1637d
daadyrbarn <daadyrbarn@gmail.com>
2019-01-23 13:16:40 +0100

    Add toilet code

---
+++ commands/code.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'code',
+	description: 'Toilet code for Espresso House!',
+	args: true,
+	aliases: ['kode'],
+
+	execute(message, args) {
+		const msg = new Discord.RichEmbed()
+			.setColor('#0099ff')
+			.addField(`Pip pip!', 'The code to the toilet is 1706. Have a nice poop, ${message.author.username}!`);
+		message.channel.send(msg);
+	},
+};
