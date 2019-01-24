6a28fae32fcf294b8a151d7f18eb24154e7575c1
daadyrbarn <daadyrbarn@gmail.com>
2019-01-24 15:17:51 +0100

    move queries into request body

--- commands/team.js
+++ commands/team.js
@@ -17,31 +17,29 @@ module.exports = {
 		}
 
 		const url = 'http://gsx2json.com/api?id=1EkZL4tGPxCgSG7-NfzLH0DtYaO6axSKeD_qNiFbSNIA&sheet=1';
-		const data = request.get(url, function(error, response, body) {
+		request.get(url, function(error, response, body) {
 			if (!error && response.statuscode == 200) {
-				const d = JSON.parse(body);
-				return d;
+				const data = JSON.parse(body);
+				if (data.columns.trainer.includes(trainer)) {
+					const trainer_data = _.where(data.rows, { trainer: trainer });
+					const teamlist = [];
+					teamlist.push(trainer_data[0].pick1);
+					teamlist.push(trainer_data[0].pick2);
+					teamlist.push(trainer_data[0].pick3);
+					teamlist.push(trainer_data[0].pick4);
+					teamlist.push(trainer_data[0].pick5);
+					teamlist.push(trainer_data[0].pick6);
+					teamlist.push(trainer_data[0].pick7);
+					teamlist.push(trainer_data[0].pick8);
+					teamlist.push(trainer_data[0].pick9);
+					teamlist.push(trainer_data[0].pick10);
+
+					message.channel.send(teamlist);
+				}
+				else {
+					message.channel.send(`No trainer found with the name ${trainer}`);
+				}
 			}
 		});
-
-		if (data.columns.trainer.includes(trainer)) {
-			const trainer_data = _.where(data.rows, { trainer: trainer });
-			const teamlist = [];
-			teamlist.push(trainer_data[0].pick1);
-			teamlist.push(trainer_data[0].pick2);
-			teamlist.push(trainer_data[0].pick3);
-			teamlist.push(trainer_data[0].pick4);
-			teamlist.push(trainer_data[0].pick5);
-			teamlist.push(trainer_data[0].pick6);
-			teamlist.push(trainer_data[0].pick7);
-			teamlist.push(trainer_data[0].pick8);
-			teamlist.push(trainer_data[0].pick9);
-			teamlist.push(trainer_data[0].pick10);
-
-			message.channel.send(teamlist);
-		}
-		else {
-			message.channel.send(`No trainer found with the name ${trainer}`);
-		}
 	},
 };
