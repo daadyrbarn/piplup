72a6164f86900106df95f9ce94baf5ea6f2f2068
daadyrbarn <daadyrbarn@gmail.com>
2019-01-27 01:17:49 +0100

    add functionality for 'has' argument

--- commands/team.js
+++ commands/team.js
@@ -10,50 +10,92 @@ module.exports = {
 	aliases: ['t'],
 
 	execute(message, args) {
-		let trainer;
-		if(args[0]) {
-			trainer = args[0];
-		}
-		else {
-			trainer = message.author.username;
-		}
+		if (args[0] == 'has') {
+			args.splice(0, 1);
+			if (!args.length) return;
+			let pokemon;
+			if (args.length == 1) {
+				pokemon = args[0];
+			}
+			else if (args.length > 1) {
+				pokemon = args.join(' ');
+			}
 
-		request.get('http://gsx2json.com/api?id=1EkZL4tGPxCgSG7-NfzLH0DtYaO6axSKeD_qNiFbSNIA&sheet=1', function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				//	console.log('Fetched JSON successfully!');
-				//	console.log(response.statusCode);
-				const data = JSON.parse(body);
-				if (data.columns.trainer.includes(trainer)) {
-					const trainer_data = _.where(data.rows, { trainer: trainer });
-					const teamlist = [];
-					teamlist.push(trainer_data[0].pick1);
-					teamlist.push(trainer_data[0].pick2);
-					teamlist.push(trainer_data[0].pick3);
-					teamlist.push(trainer_data[0].pick4);
-					teamlist.push(trainer_data[0].pick5);
-					teamlist.push(trainer_data[0].pick6);
-					teamlist.push(trainer_data[0].pick7);
-					teamlist.push(trainer_data[0].pick8);
-					teamlist.push(trainer_data[0].pick9);
-					teamlist.push(trainer_data[0].pick10);
+			request.get('http://gsx2json.com/api?id=1EkZL4tGPxCgSG7-NfzLH0DtYaO6axSKeD_qNiFbSNIA&sheet=1', function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					const data = JSON.parse(body);
+					let trainers = [];
+					for (let i; i < data.rows.length; i++) {
+						const datarow = data.rows[i];
+						if (datarow.includes(pokemon)) trainers += datarow[0];
+					}
 
-					const teamEmbed = new Discord.RichEmbed()
-						.setColor('#0099ff')
-						.addField('Pip! I know that!', `**${trainer}**'s team is:\n${teamlist.join('\n')}`)
-						.setTimestamp()
-						.setFooter(`Use ${prefix}team [trainer] to see another trainer's draft team.`);
+					if (trainers.length) {
+						const trainerEmbed = new Discord.RichEmbed()
+							.setColor('#0099ff')
+							.addField('Pip! Found it!', `The trainers with **${pokemon}** in their teams are:\n${trainers.join('\n')}`)
+							.setTimestamp()
+							.setFooter(`Use ${prefix}team has [pokemon] to see who drafted a specific pokemon.`);
 
-					message.channel.send(teamEmbed);
+						message.channel.send(trainerEmbed);
+					}
+					else {
+						message.channel.send(`Pip? I couldn't find a trainer with **${pokemon}** in their current team.`);
+					}
 				}
 				else {
-					message.channel.send(`Pip? I couldn't find a trainer with the name **${trainer}** in the current season.`);
+					message.channel.send('Piiiip! Something went wrong! Help, @daadyrbarn!');
+					console.log(error);
+					console.log(response.statusCode);
 				}
+			});
+		}
+		else {
+			let trainer;
+			if(args[0]) {
+				trainer = args[0];
 			}
 			else {
-				message.channel.send('Something went wrong! Help, @daadyrbarn!');
-				console.log(error);
-				console.log(response.statusCode);
+				trainer = message.author.username;
 			}
-		});
+
+			request.get('http://gsx2json.com/api?id=1EkZL4tGPxCgSG7-NfzLH0DtYaO6axSKeD_qNiFbSNIA&sheet=1', function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					//	console.log('Fetched JSON successfully!');
+					//	console.log(response.statusCode);
+					const data = JSON.parse(body);
+					if (data.columns.trainer.includes(trainer)) {
+						const trainer_data = _.where(data.rows, { trainer: trainer });
+						const teamlist = [];
+						teamlist.push(trainer_data[0].pick1);
+						teamlist.push(trainer_data[0].pick2);
+						teamlist.push(trainer_data[0].pick3);
+						teamlist.push(trainer_data[0].pick4);
+						teamlist.push(trainer_data[0].pick5);
+						teamlist.push(trainer_data[0].pick6);
+						teamlist.push(trainer_data[0].pick7);
+						teamlist.push(trainer_data[0].pick8);
+						teamlist.push(trainer_data[0].pick9);
+						teamlist.push(trainer_data[0].pick10);
+
+						const teamEmbed = new Discord.RichEmbed()
+							.setColor('#0099ff')
+							.addField('Pip! I know that!', `**${trainer}**'s team is:\n${teamlist.join('\n')}`)
+							.setTimestamp()
+							.setFooter(`Use ${prefix}team [trainer] to see another trainer's draft team.`);
+
+						message.channel.send(teamEmbed);
+					}
+					else {
+						message.channel.send(`Pip? I couldn't find a trainer with the name **${trainer}** in the current season.`);
+					}
+				}
+				else {
+					message.channel.send('Something went wrong! Help, @daadyrbarn!');
+					console.log(error);
+					console.log(response.statusCode);
+				}
+			});
+		}
 	},
 };
