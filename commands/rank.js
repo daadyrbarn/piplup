91369b794273835ee923076a0cdff38e02e34151
daadyrbarn <daadyrbarn@gmail.com>
2019-08-24 14:32:52 +0200

    revert to data_url var again after transferring correct config.json

--- commands/rank.js
+++ commands/rank.js
@@ -14,7 +14,7 @@ module.exports = {
 
 		// console.log(data_url);
 		if (args[0]) {
-			request.get('http://localhost:5000/api?id=1tDVFpMYSbPpXahlNo4QXyrkvj0KN1rB8uQ9E55TS19E&sheet=1', function(error, response, body) {
+			request.get(data_url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					const data = JSON.parse(body);
 
