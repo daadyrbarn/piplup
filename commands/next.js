2713dcfee7f3b841154a0b17e1689771315fa333
daadyrbarn <daadyrbarn@gmail.com>
2019-09-20 23:15:55 +0200

    new deadline for season 6

--- commands/next.js
+++ commands/next.js
@@ -4,13 +4,13 @@ module.exports = {
 	name: 'next',
 	description: 'Info about next season!',
 	execute(message, args) {
-		const date = new Date('April 1, 2019 23:59:59 GMT+01:00');
+		const date = new Date('October 1, 2019 23:59:59 GMT+01:00');
 		const now = Date.now();
 		const msLeft = date - now;
 		const daysLeft = Math.floor(msLeft / (60 * 60 * 24 * 1000) + 1);
 		const info = new Discord.RichEmbed()
 			.setColor('#0099ff')
-			.addField('Pip pip!', `Remember to sign up no later than April 1st! Only ${daysLeft} days left!\nIf you haven't already signed up, you can do it [here](https://forms.gle/iQM76AZ7g3GwK4cC6)!`);
+			.addField('Pip pip!', `Remember to sign up no later than October 1st! Only ${daysLeft} days left!\nIf you haven't already signed up, you can do it [here](https://forms.gle/HVqfrGtz1QQQw1YQA)!`);
 		message.channel.send(info);
 	},
 };
