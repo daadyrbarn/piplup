8ba490100fbbe3c50628441384ccb68a3df79f2a
daadyrbarn <daadyrbarn@gmail.com>
2019-10-26 21:57:59 +0200

    add invite link command

---
+++ commands/invite.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'invite',
+	description: 'Link to the server',
+	execute(message, args) {
+		message.channel.send('More piple? OK! Pip! \nhttps://discord.gg/uftDaMa');
+	},
+};
