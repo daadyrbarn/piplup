4bd739e8703485a3c49fab9845fce315867c4471
daadyrbarn <daadyrbarn@gmail.com>
2019-12-22 19:41:00 +0100

    update for next season

--- commands/next.js
+++ commands/next.js
@@ -4,13 +4,13 @@ module.exports = {
 	name: 'next',
 	description: 'Info about next season!',
 	execute(message, args) {
-		const date = new Date('October 1, 2019 23:59:59 GMT+01:00');
+		const date = new Date('January 1, 2020 23:59:59 GMT+01:00');
 		const now = Date.now();
 		const msLeft = date - now;
 		const daysLeft = Math.floor(msLeft / (60 * 60 * 24 * 1000) + 1);
 		const info = new Discord.RichEmbed()
 			.setColor('#0099ff')
-			.addField('Pip pip!', `Remember to sign up no later than October 1st! Only ${daysLeft} days left!\nIf you haven't already signed up, you can do it [here](https://forms.gle/HVqfrGtz1QQQw1YQA)!`);
+			.addField('Pip pip!', `Remember to sign up for Kalos League no later than January 1st! Only ${daysLeft} days left!\nIf you haven't already signed up, you can do it [here](https://forms.gle/UMSb2iBuBcMwSi8LA)!`);
 		message.channel.send(info);
 	},
 };
